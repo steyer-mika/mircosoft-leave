44aaa11e047ed881a04b1c12296917a3b07d2a94
Mika Steyer <mikasteyer9@gmail.com>
2024-02-10 15:27:34 +0100

    feat: add wpf application

---
+++ LeaveApp/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace LeaveApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
